4fd7d2ade73a82305607cea3f0e91cf88456a2d9
Amit Mittal <mittalmailbox@gmail.com>
2021-04-15 12:24:53 +0530

    updated pointer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,21 @@
 import { Flex } from "@chakra-ui/layout";
 import React, { ReactElement } from "react";
-import Link  from "next/link";
+import Link from "next/link";
 import Image from "next/image";
-
-
 
 export default function Navbar(): ReactElement {
   return (
     <Flex>
-      <Link href="/"><Flex p="2" justifyContent="center" width="full">
-        <Image src="/logo.png"  width="400px" height="auto"  alt="Dash TV Logo" />
-      </Flex></Link>
+      <Link href="/" >
+        <Flex p="2" justifyContent="center" width="full" cursor="pointer">
+          <Image
+            src="/logo.png"
+            width="400px"
+            height="auto"
+            alt="unpkg search Logo"
+          />
+        </Flex>
+      </Link>
     </Flex>
   );
 }
